4dd443752059061e24efef434d6908cadcc45f79
alinasagaydak <44638862+alinasagaydak@users.noreply.github.com>
2024-10-23 02:25:23 +0700

    Add files via upload

--- task5.C
+++ task5.C
@@ -3,46 +3,51 @@
 #include <TH1.h>
 #include <TF1.h>
 #include <TFile.h>
+#include <TString.h>
 
 // C++ libraries
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <thread>
 
 Double_t inv_func(Double_t x){ return sqrt(-log(1-x)); }
 
 Double_t myfunc(Double_t x) { return exp(-x) * pow(x, 3); }
 
 Double_t AverageValMethod(Double_t (*myfunc)(Double_t x), Double_t N_sample) {
+    TRandom3* rand = new TRandom3(0);
     Double_t sum = 0;
     for (size_t i = 0; i < N_sample; i++) {
-        Double_t r = gRandom->Rndm();
+        Double_t r = rand->Rndm();
         sum += (*myfunc)(r);
     }
     return 1 / N_sample * sum;
 }
 
 Double_t GeometricAlgorithm(Double_t (*myfunc)(Double_t x), Double_t N_sample) {
+    TRandom3* rand = new TRandom3(0);
     Double_t M = (*myfunc)(1);
     Double_t N_selected = 0.;
     for (size_t i = 0; i < N_sample; i++) {
-        Double_t eta = gRandom->Rndm(); // x
-        Double_t xi = M * gRandom->Rndm(); // y
-        if (abs(xi - (*myfunc)(eta)) <= pow(10, -3)) N_selected += 1.;
+        Double_t eta = rand->Rndm(); // x
+        Double_t xi = M * rand->Rndm(); // y
+        if (xi <=  (*myfunc)(eta)) N_selected++;
     }
     return (N_selected / N_sample) * M;
 }
 
 Double_t MainPartIntegration(Double_t N_sample) {
     //Double_t N_selected = 0.;
+    TRandom3* rand = new TRandom3(0);
     Double_t sum = 0.;
     for (size_t i = 0; i < N_sample; i++) {
-        Double_t r = gRandom->Rndm();
+        Double_t r = rand->Rndm();
         Double_t xi = pow(r, 0.25);
         //Double_t x = gRandom->Rndm();
         //Double_t y = gRandom->Rndm();
         //if (xi < exp(r)) N_selected += 1;
-        sum += exp(xi);
+        sum += exp(xi)/4;
     }
     return 1 / N_sample * sum;
     //return N_selected / N_sample;
@@ -64,24 +69,69 @@ void PrintResults(Double_t& resAverVal, Double_t& resGeomAlg, Double_t& resMainP
 }
 
 void task5() {
+    Double_t N_sample = pow(10, 5);
+    Int_t num_itrs = 1000;
+    std::vector<Double_t> resultsVec(num_itrs * 3);
+
+    // Сразу запускаем потоки, чтобы программа быстрее завершилась. Три вида интегрирования, набираем данные    
+    std::thread th1([&resultsVec, &N_sample, &num_itrs]()
+    {
+      for (int i = 0; i < num_itrs; i++) {
+        Double_t res = AverageValMethod((*myfunc), N_sample);
+        resultsVec[i] = res;  
+      }             
+    });
+
+    std::thread th2([&resultsVec, &N_sample, &num_itrs]()
+    {
+        for (size_t i = num_itrs; i < 2 * num_itrs; i++) {
+            Double_t res = GeometricAlgorithm((*myfunc), N_sample);
+            resultsVec[i] = res;
+        }     
+    });
+
+    std::thread th3([&resultsVec, &N_sample, &num_itrs]()
+    {
+        for (size_t i = 2 * num_itrs; i < 3 * num_itrs; i++) {
+            Double_t res = MainPartIntegration(N_sample);
+            resultsVec[i] = res;
+        }        
+    });
+
     // метод обратных функций f(x) = 2*x*exp(-x^2)
     TH1D* h_invfunc = new TH1D("h_invfunc", "The method of inverse functions", 150, 0., 3.5);
-
-    for (size_t i = 0; i < 1000000; i++) {
-        Double_t r = gRandom->Rndm();
-        Double_t xi = inv_func(r);
-        h_invfunc->Fill(xi);
+    for (size_t i = 0; i < N_sample; i++) {
+         Double_t r = gRandom->Rndm();
+         Double_t xi = inv_func(r);
+         h_invfunc->Fill(xi);
     }
-    
+
     // вычисление интеграла от 0 до 1 функции x^3 * exp(-x)
-    Double_t resAverVal = AverageValMethod((*myfunc), 1000000.);
-    Double_t resGeomAlg = GeometricAlgorithm((*myfunc), 1000000.);
-    Double_t resMainPartInt = MainPartIntegration(1000000.);
+    Double_t resAverVal = AverageValMethod((*myfunc), N_sample);
+    Double_t resGeomAlg = GeometricAlgorithm((*myfunc), N_sample);
+    Double_t resMainPartInt = MainPartIntegration(N_sample);
 
-    std::vector<Double_t> errors = MethodsError(1000000.);
-
+    std::vector<Double_t> errors = MethodsError(N_sample);
     PrintResults(resAverVal, resGeomAlg, resMainPartInt, errors);
 
+
+    th1.join();
+    th2.join();
+    th3.join();
+
+    TH1D* h_resAverVal = new TH1D("h_resAverVal", Form("Average Value Method Results, N_sample = %0.f", N_sample), 100, 0.11, 0.12);
+    std::for_each(resultsVec.begin(), resultsVec.begin() + num_itrs, [&h_resAverVal](Double_t& res){h_resAverVal->Fill(res);});
+
+    TH1D* h_resGeomAlg = new TH1D("h_resGeomAlg", Form("Geometric Algorithm Results, N_sample = %0.f", N_sample), 100, 0.11, 0.12);
+    std::for_each(resultsVec.begin() + num_itrs, resultsVec.begin() + 2*num_itrs, [&h_resGeomAlg](Double_t& res){h_resGeomAlg->Fill(res);});
+    
+    TH1D* h_resMainPartInt = new TH1D("h_resMainPartInt", Form("Main Part Integration Method Results, N_sample = %0.f", N_sample), 100, 0.56, 0.57);
+    std::for_each(resultsVec.begin() + 2*num_itrs, resultsVec.end(), [&h_resMainPartInt](Double_t& res){h_resMainPartInt->Fill(res);});
+
+
     TFile* ofile = new TFile("ofile.root", "RECREATE");
-    h_invfunc->Write();    
+    h_invfunc->Write();
+    h_resAverVal->Write();
+    h_resGeomAlg->Write();
+    h_resMainPartInt->Write();
 }
