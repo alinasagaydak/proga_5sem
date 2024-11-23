55f024262db89b41a00eb61d49e9a2acf5fde36b
alinasagaydak <44638862+alinasagaydak@users.noreply.github.com>
2024-11-23 07:54:28 -0500

    Add files via upload

--- task7.C
+++ task7.C
@@ -5,8 +5,8 @@ void task7() {
     TFile ifile("m3pimc.root", "read");
     TTree* h10 = (TTree*)ifile.Get("h10");
     int nph; Float_t eph;
-    Float_t thetaph[nph];
-    Float_t phiph[nph];
+    float thetaph[50];
+    float phiph[50];
     unsigned char Isrfilter;
     float chi2_3p;
 
@@ -21,30 +21,31 @@ void task7() {
     TFile* ofile = new TFile("newroot.root", "RECREATE");
     TTree* MyTree = new TTree("MyTree", "tree with data");
     int my_nph; Float_t my_eph;
-    Float_t my_thetaph[my_nph];
-    Float_t my_phiph[my_nph];
+    float my_thetaph[50];
+    float my_phiph[50];
 
     MyTree->Branch("nph", &my_nph);
     MyTree->Branch("eph", &my_eph);
-    MyTree->Branch("thetaph", my_thetaph, "my_thetaph[my_nph]/F");
-    MyTree->Branch("phiph", my_phiph, "my_phiph[my_nph]/F");
+    MyTree->Branch("thetaph", my_thetaph, "my_thetaph[50]/F");
+    MyTree->Branch("phiph", my_phiph, "my_phiph[50]/F");
 
     for (int i = 0; i < h10->GetEntries(); i++) {
         h10->GetEntry(i);
-        my_nph = nph;
-        my_eph = eph;
-        if (Isrfilter == 1 && chi2_3p < 30) {
+        if (nph == 0) continue;
+        else if (Isrfilter == 1 && chi2_3p < 30) {
+            my_nph = nph;
+            my_eph = eph;
             for (int j; j < nph; j++) {
                 my_thetaph[j] = thetaph[j];
                 my_phiph[j] = phiph[j];
+                MyTree->Fill();
             }
         } 
-        MyTree->Fill();
     }
 
-    ifile.Close();
     
     ofile->cd();
     MyTree->Write();
     ofile->Close();
+    ifile.Close();
 }
